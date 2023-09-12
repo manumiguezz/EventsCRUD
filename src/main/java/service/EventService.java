6ce5d8d8a7494e08dad0eac6e0312d50ec62905e
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-09-12 13:28:13 -0300

    Event service class created and methods defined

---
+++ src/main/java/service/EventService.java
@@ -0,0 +1,16 @@
+package service;
+
+import entity.Event;
+
+import java.util.List;
+
+public interface EventService {
+
+    public List<Event> findAll();
+
+    public Event findById(int theId);
+
+    public void save(Event theEvent);
+
+    public void deleteById (int theId);
+}
