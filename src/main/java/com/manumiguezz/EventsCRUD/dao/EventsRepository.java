aa017562d6eae640fad4ffe4ea940c40b0b37d61
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-09-13 11:42:45 -0300

    bug fixed

--- src/main/java/com/manumiguezz/EventsCRUD/dao/EventsRepository.java
+++ src/main/java/com/manumiguezz/EventsCRUD/dao/EventsRepository.java
@@ -3,5 +3,4 @@ package com.manumiguezz.EventsCRUD.dao;
 import com.manumiguezz.EventsCRUD.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EventsRepository extends JpaRepository<Event, > {
-}
+public interface EventsRepository extends JpaRepository<Event, Integer> {}
