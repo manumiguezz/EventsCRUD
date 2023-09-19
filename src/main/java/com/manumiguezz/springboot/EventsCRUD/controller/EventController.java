353c542a1dbf9f645b835e4b57ebefdc91cf9585
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-09-19 19:29:35 -0300

    button added and form updated with process data on controller

--- src/main/java/com/manumiguezz/springboot/EventsCRUD/controller/EventController.java
+++ src/main/java/com/manumiguezz/springboot/EventsCRUD/controller/EventController.java
@@ -7,6 +7,8 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -38,5 +40,12 @@ public class EventController {
         theModel.addAttribute("event", theEvent);
 
         return "events/event-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEvent(@ModelAttribute("event") Event theEvent){
+        eventService.save(theEvent);
+
+        return "redirect:/events/list";
     }
 }
