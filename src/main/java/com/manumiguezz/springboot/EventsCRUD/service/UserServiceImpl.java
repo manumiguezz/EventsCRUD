faf40baeb67960561d83ad91fbe42d263e2d4930
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-09-26 11:48:21 -0300

    user service impl updated with userdetails and granted authority

--- src/main/java/com/manumiguezz/springboot/EventsCRUD/service/UserServiceImpl.java
+++ src/main/java/com/manumiguezz/springboot/EventsCRUD/service/UserServiceImpl.java
@@ -1,10 +1,16 @@
 package com.manumiguezz.springboot.EventsCRUD.service;
 
+import com.manumiguezz.springboot.EventsCRUD.dao.RoleDao;
 import com.manumiguezz.springboot.EventsCRUD.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class UserServiceImpl implements UserService{
 
@@ -26,6 +32,16 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userDao.findByUserName(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Invalid username or password");
+        }
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(), user.getPassword(), mapRolesToAuthorities(user.getAuthorities())
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities (Collection<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
     }
 }
