84641677f9d1ef4c2e8cfc737aa2be67ded6cf3c
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-09-26 11:22:14 -0300

    user service created

---
+++ src/main/java/com/manumiguezz/springboot/EventsCRUD/service/UserService.java
@@ -0,0 +1,9 @@
+package com.manumiguezz.springboot.EventsCRUD.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+
+    public User findByUserName (String userName);
+}
