a07a6453e34ce76f953b24790878ea4b06fb592a
Manuel Miguez <manuelmiguezlauria@gmail.com>
2023-09-26 11:19:35 -0300

    role class added to entity

---
+++ src/main/java/com/manumiguezz/springboot/EventsCRUD/entity/Role.java
@@ -0,0 +1,37 @@
+package com.manumiguezz.springboot.EventsCRUD.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Role(int id, String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
